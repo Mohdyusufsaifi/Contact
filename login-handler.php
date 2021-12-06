9f4f550118e6c43a34857f5a7c3539cf6e65f13b
Mohdyusufsaifi <95411943+Mohdyusufsaifi@users.noreply.github.com>
2021-12-06 12:04:47 +0530

    Update login-handler.php

--- login-handler.php
+++ login-handler.php
@@ -3,12 +3,12 @@ $name =$_POST['name'];
 $visitor_email = $_POST['email'];
 $message= $_POST['message'];
 
-$email_form ='https://mohdyusufsaifi.github.io/Contact/Login%20page.html';
+$email_form ='azeemsaifi38180@gmail.com';
 $email_subject="New idea association";
 $email_body="User Name: $name.\n".
 "User Email:$vistor_email.\n".
 "User Message:$message.\n";
-$to ="azeemsaifi38180@gmail.com";
+$to ="Loginpage";
 $headers="From: $email_from\r\n";
 $headers .="Reply-To: $visitor_email  \r\n";
 mail($to,$email_subject,$email_body,$headers);
